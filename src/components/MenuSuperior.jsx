fa885102e6705a2585e5a08f25e6a1175fbb61e2
yrkaique <kaiquebjesus22@gmail.com>
2026-09-23 17:13:24 -0300

    kaique menu-superior

--- src/components/MenuSuperior.jsx
+++ src/components/MenuSuperior.jsx
@@ -1,11 +1,24 @@
 import './MenuSuperior.css'
 
-function MenuSuperior(){
+function MenuSuperior() {
     return (
         <div>
-            <a href="#">Início </a>
-            <a href="#">Produto </a>
-            <a href="#">Contato</a>
+            <nav className="navbar">
+                <div className="logo-container">
+                    <img src="../logosfundo.png" alt="Sportincity Logo" />
+                </div>
+                <ul className="nav-links">
+                    <li><a href="#">Início</a></li>
+                    <li><a href="#">Quadras</a></li>
+                    <li><a href="#" >Filiais</a></li>
+                    <li><a href="#">Torneios</a></li>
+                </ul>
+                <div className="search-container">
+                    <input type="text" placeholder="Pesquisar..." className="search-input" id="busca" />
+                    <button className="search-btn" aria-label="Buscar">🔍</button>
+                </div>
+                <button className="btn-login" id="btn-login">Login</button>
+            </nav>
         </div>
     );
 }
