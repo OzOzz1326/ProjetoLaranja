edffde455882cc5c5c9354fb54678ce7fdfb4b30
yrkaique <kaiquebjesus22@gmail.com>
2026-09-24 15:04:16 -0300

    atualização

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import MenuSuperior from './components/MenuSuperior.jsx'
+import Rodape from './components/Rodape.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <MenuSuperior/>
     <App />
+    <Rodape/>
   </StrictMode>,
 )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import "./Login.css"
 
-function Login(){
-    return(
+function Login() {
+    return (
         <div>
             <main className="container-login">
                 <div className="card-login">
@@ -17,21 +17,21 @@ function Login(){
                         <label htmlFor="email">Telefone/Email:</label>
                         <input type="text" id="email" className="input-login" placeholder="Telefone ou Email" required />
 
-                            <label htmlFor="senha">Senha:</label>
-                            <input type="password" id="senha" className="input-login" placeholder="Senha" required />
+                        <label htmlFor="senha">Senha:</label>
+                        <input type="password" id="senha" className="input-login" placeholder="Senha" required />
 
-                                <div className="botao-login">
-                                    <button type="submit" className="btn-entrar">Entrar</button>
-                                </div>
+                        <div className="botao-login">
+                            <button type="submit" className="btn-entrar">Entrar</button>
+                        </div>
 
-                                <a href="#" className="esqueci-senha">Esqueci minha senha.</a>
+                        <a href="#" className="esqueci-senha">Esqueci minha senha.</a>
 
-                                <p className="cadastro">Não possui uma conta? <a href="cadastro.html">Cadastre-se aqui!</a></p>
+                        <p className="cadastro">Não possui uma conta? <a href="cadastro.html">Cadastre-se aqui!</a></p>
                     </form>
-            </div>
+                </div>
             </main>
-            </div>
-        );
+        </div>
+    );
 }
 
 export default Login;
